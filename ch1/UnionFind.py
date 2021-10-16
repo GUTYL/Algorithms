44e0af3bf441aef575a5cfd1d2406c82957b9d14
GUTYL <yang.li.gut@foxmail.com>
2021-10-16 12:58:04 +0800

    添加chapter1.5.2.1 quick-find

--- ch1/UnionFind.py
+++ ch1/UnionFind.py
@@ -1,25 +1,56 @@
 class UnionFind:
     def __init__(self, n: int):
-        self.id = [i for i in range(n)]
-        self.count = n
+        # 分量id
+        self._id = [i for i in range(n)]
+        # 分量数量
+        self._count = n
 
-    def connected(self, p: int, q: int) -> bool:
-        return self.find(p) == self.find(q)
+    def connected_quick_find(self, p: int, q: int) -> bool:
+        return self.find_quick_find(p) == self.find_quick_find(q)
 
-    def find(self, p: int) -> int:
-        pass
+    def find_quick_find(self, p: int) -> int:
+        return self._id[p]
 
-    def union(self, p: int, q: int):
-        pass
+    def count(self):
+        return self._count
+
+    def union_quick_find(self, p: int, q: int):
+        p_id = self._id[p]
+        q_id = self._id[q]
+        if p_id == q_id:
+            return
+        # 将p的分量重命名为q的名称
+        for i in range(len(self._id)):
+            if self._id[i] == p_id:
+                self._id[i] = q_id
+        self._count -= 1
 
 
-if __name__ == '__main__':
+def main_read_file():
+    with open('../algs4-data/tinyUF.txt', 'r') as f:
+        n = int(f.readline().strip())
+        uf = UnionFind(n)
+        while i := f.readline():
+            p, q = map(int, i.split())
+            if uf.connected_quick_find(p, q):
+                continue
+            uf.union_quick_find(p, q)
+            print(f"{p}  {q}")
+        print(f"{uf.count()} components")
+
+
+def main_input():
     n = int(input())
     uf = UnionFind(n)
     while in_str := input():
         p, q = map(int, in_str.split())
-        if uf.connected(p, q):
+        if uf.connected_quick_find(p, q):
             continue
-        uf.union(p, q)
+        uf.union_quick_find(p, q)
         print(p + " " + q)
-    print(f"{uf.count} components")
+    print(f"{uf.count()} components")
+
+
+if __name__ == '__main__':
+    # quick_find 算法
+    main_read_file()
