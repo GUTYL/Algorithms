cacbac2a4fdafac5db6c7e6ec7c0556b6fc77d98
GUTYL <yang.li.gut@foxmail.com>
2021-10-25 23:32:16 +0800

    添加chapter1.5，quick-union算法实现

--- ch1/UnionFind.py
+++ ch1/UnionFind.py
@@ -1,20 +1,22 @@
-class UnionFind:
+class QuickFind:
+    """quick-find算法实现"""
+
     def __init__(self, n: int):
         # 分量id
         self._id = [i for i in range(n)]
         # 分量数量
         self._count = n
 
-    def connected_quick_find(self, p: int, q: int) -> bool:
-        return self.find_quick_find(p) == self.find_quick_find(q)
+    def connected(self, p: int, q: int) -> bool:
+        return self.find(p) == self.find(q)
 
-    def find_quick_find(self, p: int) -> int:
+    def find(self, p: int) -> int:
         return self._id[p]
 
     def count(self):
         return self._count
 
-    def union_quick_find(self, p: int, q: int):
+    def union(self, p: int, q: int):
         p_id = self._id[p]
         q_id = self._id[q]
         if p_id == q_id:
@@ -26,31 +28,68 @@ class UnionFind:
         self._count -= 1
 
 
+class QuickUnion:
+    """quick-union算法实现"""
+
+    def __init__(self, n: int):
+        # 分量id
+        self._id = [i for i in range(n)]
+        # 分量数量
+        self._count = n
+
+    def connected(self, p: int, q: int) -> bool:
+        return self.find(p) == self.find(q)
+
+    def find(self, p: int) -> int:
+        # 找出分量的名称
+        while p != self._id[p]:
+            p = self._id[p]
+        return p
+
+    def count(self):
+        return self._count
+
+    def union(self, p: int, q: int):
+        # 将p和q的根节点统一
+        p_root = self.find(p)
+        q_root = self.find(q)
+        if p_root == q_root:
+            return
+        self._id[p_root] = self._id[q_root]
+        self._count -= 1
+
+
 def main_read_file():
+    """从文件中读取数据"""
     with open('../algs4-data/tinyUF.txt', 'r') as f:
         n = int(f.readline().strip())
-        uf = UnionFind(n)
+        # 1.5.2.1 quick-find算法
+        # uf = QuickFind(n)
+
+        # 1.5.2.3 quick-union算法
+        uf = QuickUnion(n)
         while i := f.readline():
             p, q = map(int, i.split())
-            if uf.connected_quick_find(p, q):
+            if uf.connected(p, q):
                 continue
-            uf.union_quick_find(p, q)
+            uf.union(p, q)
             print(f"{p}  {q}")
         print(f"{uf.count()} components")
 
 
 def main_input():
+    """从输入中获取数据"""
     n = int(input())
-    uf = UnionFind(n)
+    # 1.5.2.1 quick-find算法
+    uf = QuickFind(n)
     while in_str := input():
         p, q = map(int, in_str.split())
-        if uf.connected_quick_find(p, q):
+        if uf.connected(p, q):
             continue
-        uf.union_quick_find(p, q)
-        print(p + " " + q)
+        uf.union(p, q)
+        print(f"{p}  {q}")
     print(f"{uf.count()} components")
 
 
 if __name__ == '__main__':
-    # quick_find 算法
     main_read_file()
