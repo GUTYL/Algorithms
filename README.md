831d2e9436594b01440078de8622f5e5cd992f5e
GUTYL <yang.li.gut@foxmail.com>
2021-10-26 23:18:31 +0800

    1、添加chapter1.5，加权quick-union算法实现 2、.gitignore、README.md文件修改。

--- README.md
+++ README.md
@@ -1,3 +1,3 @@
 # Algorithms
-Algorithms, 4th Edition；算法第四版的python3.9实现；[**Download test data files**](https://algs4.cs.princeton.edu/code/algs4-data.zip) 
+Algorithms, 4th Edition；算法第四版的Python3.9实现；[**Download test data files**](https://algs4.cs.princeton.edu/code/algs4-data.zip) 
 
--- ch1/UnionFind.py
+++ ch1/UnionFind.py
@@ -1,20 +1,34 @@
-class QuickFind:
-    """quick-find算法实现"""
+class UnionFind:
+    """union-find算法"""
 
     def __init__(self, n: int):
-        # 分量id
+        # 分量id，初始化，以触点作为索引
         self._id = [i for i in range(n)]
         # 分量数量
         self._count = n
 
     def connected(self, p: int, q: int) -> bool:
+        """如果p和q存在于同一个分量中则返回 True"""
         return self.find(p) == self.find(q)
+
+    def count(self) -> int:
+        """连通分量的数量"""
+        return self._count
+
+    def find(self, p: int) -> int:
+        """p（0到 n-1）所在的分量的标识符"""
+        pass
+
+    def union(self, p: int, q: int):
+        """在p和q之间添加一条连接"""
+        pass
+
+
+class QuickFind(UnionFind):
+    """quick-find算法实现"""
 
     def find(self, p: int) -> int:
         return self._id[p]
-
-    def count(self):
-        return self._count
 
     def union(self, p: int, q: int):
         p_id = self._id[p]
@@ -28,17 +42,8 @@ class QuickFind:
         self._count -= 1
 
 
-class QuickUnion:
+class QuickUnion(UnionFind):
     """quick-union算法实现"""
-
-    def __init__(self, n: int):
-        # 分量id
-        self._id = [i for i in range(n)]
-        # 分量数量
-        self._count = n
-
-    def connected(self, p: int, q: int) -> bool:
-        return self.find(p) == self.find(q)
 
     def find(self, p: int) -> int:
         # 找出分量的名称
@@ -46,16 +51,41 @@ class QuickUnion:
             p = self._id[p]
         return p
 
-    def count(self):
-        return self._count
-
     def union(self, p: int, q: int):
         # 将p和q的根节点统一
         p_root = self.find(p)
         q_root = self.find(q)
         if p_root == q_root:
             return
-        self._id[p_root] = self._id[q_root]
+        self._id[p_root] = q_root
+        self._count -= 1
+
+
+class WeightedQuickUnion(UnionFind):
+    """加权quick-union算法实现"""
+
+    def __init__(self, n: int):
+        super(WeightedQuickUnion, self).__init__(n)
+        self._sz = [i for i in range(n)]
+
+    def find(self, p: int) -> int:
+        # 跟随链接找到跟节点
+        while p != self._id[p]:
+            p = self._id[p]
+        return p
+
+    def union(self, p: int, q: int):
+        i = self.find(p)
+        j = self.find(q)
+        if i == j:
+            return
+        # 将小树的根节点连接到大树的根节点
+        if self._sz[i] < self._sz[j]:
+            self._id[i] = j
+            self._sz[j] += self._sz[i]
+        else:
+            self._id[j] = i
+            self._sz[i] += self._sz[j]
         self._count -= 1
 
 
@@ -67,7 +97,10 @@ def main_read_file():
         # uf = QuickFind(n)
 
         # 1.5.2.3 quick-union算法
-        uf = QuickUnion(n)
+        # uf = QuickUnion(n)
+
+        # 1.5.2.6 加权quick-union算法
+        uf = WeightedQuickUnion(n)
         while i := f.readline():
             p, q = map(int, i.split())
             if uf.connected(p, q):
