88bba63bd24667985c8e1596329a625eca41957b
GUTYL <yang.li.gut@foxmail.com>
2021-11-16 22:47:23 +0800

    【剑指offer】更新ch9面试题59

--- sword-for-offer/ch9_heap/Q59.py
+++ sword-for-offer/ch9_heap/Q59.py
@@ -1,1 +1,26 @@
 import heapq
+from typing import List
+
+class KthLargest:
+
+    def __init__(self, k: int, nums: List[int]):
+        self.lens = k
+        self.nums = []
+        heapq.heapify(self.nums)
+        for i in nums:
+            self.add(i)
+
+    def add(self, val: int) -> int:
+        if len(self.nums) < self.lens:
+            heapq.heappush(self.nums, val)
+        elif val > self.nums[0]:
+            heapq.heappop(self.nums)
+            heapq.heappush(self.nums, val)
+        return self.nums[0]
+
+
+
+
+# Your KthLargest object will be instantiated and called as such:
+# obj = KthLargest(k, nums)
+# param_1 = obj.add(val)
