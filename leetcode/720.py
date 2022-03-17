0db29b6e2437d5a7ef5cb966a592824434ee3976
Young <yang.li.gut@foxmail.com>
2022-03-17 23:05:39 +0800

    【leetcode】添加720

---
+++ leetcode/720.py
@@ -0,0 +1,20 @@
+from typing import List
+
+
+class Solution:
+    def longestWord(self, words: List[str]) -> str:
+        words_set = set(words)
+        result = ''
+        for word in words_set:
+            time = 0
+            for i in range(len(word)):
+                if word[:i + 1] not in words_set:
+                    continue
+                time += 1
+            if time == len(word) and len(word) >= len(result):
+                if not(len(word) == len(result) and word > result):
+                    result = word
+        return result
+
+s = Solution()
+print(s.longestWord(["a", "banana", "app", "appl", "ap", "apply", "apple"]))
