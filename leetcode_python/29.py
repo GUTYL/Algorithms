163b73f1cc7c4ac1b47681636df17c2f73005db1
Young <yang.li.gut@foxmail.com>
2022-05-01 15:55:27 +0800

    【leetcode_python】添加29

--- leetcode_python/29.py
+++ leetcode_python/29.py
@@ -1,32 +1,23 @@
-class Solution【未完成】:
-    def process1(self, dividend: int, divisor: int):
-        """dividend > 0, divisor > 0"""
-        result = -1 if divisor < 0 else 1
-        tmp = 0
-        while True:
-            if tmp < dividend:
-                tmp += divisor
-                result += 1
-            elif tmp >= dividend:
-                return result
-
+class Solution:
     def divide(self, dividend: int, divisor: int) -> int:
-
-        if divisor == 1:
-            return 1
-        if divisor == -1:
-            return -dividend
-        if dividend < divisor:
+        if not dividend:
             return 0
-        if dividend > 0 and divisor > 0:
-            return self.process1(-dividend, -divisor)
-        divmod(10, -3)
-        return self.process2(dividend, divisor)
+        if dividend == -2 ** 31 and divisor == -1:
+            return 2 ** 31 - 1
+        flag = 0 if (dividend > 0 and divisor < 0) or (dividend < 0 and divisor > 0) else 1
+        dividend, divisor = abs(dividend), abs(divisor)
+        result = 0
+        for n in range(31, -1, -1):
+            if (dividend >> n) >= divisor:
+                result += 1 << n
+                dividend -= divisor << n
+                continue
+        return result if flag else -result
 
 
-# s = Solution()
-# print(s.divide(10, 3))
-# print(s.divide(-1, 1))
-# print(s.divide(1, 1))
-print(-2**31)
-print(10/-3)
+s = Solution()
+print(s.divide(10, 3))
+print(s.divide(-1, 1))
+print(s.divide(1, 1))
+print(s.divide(-2, 31))
+print(s.divide(10, -3))
